e9bbe14d9759651b7959fe6653216fff668468ec
Alev03YT <55165974+Alev03YT@users.noreply.github.com>
2026-07-29 23:35:52 +0200

    Collega EscapeVerse al progetto Firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,14 @@
-// Configurazione Firebase di EscapeVerse.
-// I dati di configurazione web Firebase non sono password, ma il progetto deve
-// essere protetto con Firebase Authentication e Firestore Security Rules.
+// Configurazione Firebase pubblica di EscapeVerse.
+// La sicurezza è gestita da Authentication e dalle regole Firestore.
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyC5u0REjnR80-Ur28ZX5tNwbeKw306tSpM",
+  authDomain: "escapeverse-ba4d0.firebaseapp.com",
+  projectId: "escapeverse-ba4d0",
+  storageBucket: "escapeverse-ba4d0.firebasestorage.app",
+  messagingSenderId: "978479405035",
+  appId: "1:978479405035:web:903f8d4bbb87d60f366bae",
+  measurementId: "G-E0EEDJFT58"
 };
 
 export const firebaseConfigured = Object.values(firebaseConfig).every(Boolean);
+export const ADMIN_EMAIL = "alessandro.valerio03@gmail.com";
